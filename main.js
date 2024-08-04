22db7c07cc3e0b255dd5be82c96624e36362fe9e
Bryan-CoderS <139633909+Bryan-CoderS@users.noreply.github.com>
2024-08-04 20:23:16 +0800

    Header for Residence
    + alternate header for residence
    + function to switch header

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
 const login = document.querySelector(".login");
-const closeBut = document.querySelector(".close");
+const headerGuest = document.querySelector(".headerGuest");
+const headerRes = document.querySelector(".headerRes");
+
+const close_but = document.querySelector(".close");
 const res_login = document.querySelector(".res_login");
+const logButton = document.querySelector(".logButton");
+
+var log_state = false;
 
 res_login.addEventListener("click", () => {
   login.style.display = "flex";
 });
 
-closeBut.addEventListener("click", () => {
+close_but.addEventListener("click", () => {
   login.style.display = "none";
 });
 
+logButton.addEventListener("click", () => {
+  headerGuest.style.display = "none";
+  headerRes.style.display = "flex";
+});
