39348d82ff0f238f7255e4ffdfbc76f5669db8e1
Bryan-CoderS <139633909+Bryan-CoderS@users.noreply.github.com>
2024-08-05 21:24:22 +0800

    Contact Window update
    + close button
    + "Contact Us" on residence header also works now
    + headerRes => headerResidence

--- main.js
+++ main.js
@@ -1,27 +1,31 @@
 const contact = document.querySelector(".contact");
 const login = document.querySelector(".login");
 const headerGuest = document.querySelector(".headerGuest");
-const headerRes = document.querySelector(".headerRes");
+const headerResidence = document.querySelector(".headerResidence");
 
 const res_login = document.querySelector(".res_login");
-const cont_page = document.querySelector(".cont_page");
+const cont_page = document.querySelectorAll(".cont_page");
 const logButton = document.querySelector(".logButton");
-const close_but = document.querySelector(".close");
+const close_but = document.querySelectorAll(".close");
 
 res_login.addEventListener("click", () => {
   login.style.display = "flex";
 });
 
-cont_page.addEventListener("click", () => {
-  contact.style.display = "flex";
-});
-
-close_but.addEventListener("click", () => {
-  login.style.display = "none";
+close_but.forEach((button) => { //forEach calls elements of an array separately
+  button.addEventListener("click", () => {  //addEventListener only works when one node is called
+    button.parentNode.style.display = "none";
+  });
 });
 
 logButton.addEventListener("click", () => {
   login.style.display = "none";
   headerGuest.style.display = "none";
-  headerRes.style.display = "block";
+  headerResidence.style.display = "block";
+});
+
+cont_page.forEach((page) => {
+  page.addEventListener("click", () => {
+    contact.style.display = "flex";
+  });
 });
